907ecbb4c8e0921dc748bad96eca34efe711f998
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2013-03-04 11:40:35 +0000

    Adjust CSS settings file refs #18
    Former-commit-id: 4c616650654bcc374f0f18c385a9a5088fc04fc6

--- CSS/start_css_motors.bat
+++ CSS/start_css_motors.bat
@@ -6,6 +6,5 @@ set CSS_SHARE=%MYLOCDIR%Share
 REM this defines MYDIR
 call %MYLOCDIR%..\config_env.bat
 start /min caRepeater.exe
-@echo on
 cd /d %MYLOCDIR%Workspaces
 %CSS_DIR%\CSS\css.exe -pluginCustomization %MYLOCDIR%Config\settings.ini -share_link %CSS_SHARE%=/CSS/Share -data %MYLOCDIR%Workspaces\motors --launcher.openFile "%MYLOCDIR%\Share\motor_opi\topMotors4.opi"
