add0eaf9098cdd78eee219b3fbe7f318dc21cc07
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2013-03-18 14:50:00 +0000

    Build doxygen on Linux
    Former-commit-id: 99d696089ce8fe5823dc667fd8ecdadeda6ed38c

--- build.sh
+++ build.sh
@@ -2,3 +2,9 @@
 set -o errexit
 . ./config_env.sh
 make $*
+cd ISIS/lvDCOM
+doxygen Doxyfile
+if test -d /isis/www/EPICS/lvDCOM; then
+    rm -fr /isis/www/EPICS/lvDCOM/html
+    cp -r doc/html /isis/www/EPICS/lvDCOM
+fi
