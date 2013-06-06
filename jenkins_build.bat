1c76b6186239401d666cfdbdb9abda6993c32451
Matt Clarke <matt.clarke@stfc.ac.uk>
2013-06-06 10:28:54 +0000

    Working on ticket #133. Made the bat file a bit more robust.
    Former-commit-id: a995940a94750aea1d2722dc7c82d9b5c9c5808c

--- jenkins_build.bat
+++ jenkins_build.bat
@@ -1,14 +1,31 @@
 REM Argument 1 = directory to link to (e.g. %WORKSPACE%\EPICS)
 REM Argument 2 = name for zip file (e.g. EPICS_win7_x64.zip)
 REM Argument 3 = architecture (defaults to x64)
-REM Argument 4 = static (defaults to not static)
-REM For example, in Jenkins the build command might be: call jenkins_build.bat %WORKSPACE%\EPICS EPICS_win7_x64.zip
+REM Argument 4 = static (defaults to shared)
+
+REM Example Jenkins build commands: 
+
+REM cd %WORKSPACE%\EPICS
+REM call jenkins_build.bat %WORKSPACE%\EPICS EPICS_win7_x64.zip
+
+REM cd %WORKSPACE%\EPICS
+REM call jenkins_build.bat %WORKSPACE%\EPICS EPICS_STATIC_win7_x64.zip windows-x64 static
 
 cd %1
 mklink /d C:\EPICS %1
 
+set arch_type=windows-x64
+set build_type=shared
+
+if NOT "%3" == "" (
+    set arch_type=%3
+)
+if NOT "%4" == "" (
+    set build_type=%4
+)
+
 cd c:\EPICS
-call build.bat %3 %4
+call build.bat arch_type build_type
 
 C:\"Program Files"\7-Zip\7z.exe a -xr!.svn -xr!CSS -xr!pilot -xr!src %2 *
 move /Y %2 c:\Installers\EPICS\.
